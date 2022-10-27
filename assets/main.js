318da3380b0756c7c522390e30d9ceecb1a66561
simla04 <jessicasowi@gmail.com>
2022-10-27 05:29:55 +0200

    .

---
+++ assets/main.js
@@ -0,0 +1,41 @@
+/*
+Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.
+Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.============================================================================================================================
+*/ 
+// let arrayNumber = [];
+
+
+// //soldatino
+// let sum = 0;
+
+// while( sum <= 50){
+//     let numberUtente = parseInt(prompt("inserire un numero:"));
+//     sum += numberUtente;
+//     arrayNumber.push(numberUtente);
+    
+// }
+
+// console.log(sum)
+// console.log(arrayNumber)
+
+
+
+
+/*
+Il software deve chiedere per 5 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+===========================================================================================================================
+*/ 
+
+let sumArray = 0;
+let sum = 0;
+
+while ( sum < i++) {
+
+    let number = parseInt(prompt("inserire un numero:"));
+        sum += number;
+    sumArray++;
+}
+console.log(sum);
+
+
